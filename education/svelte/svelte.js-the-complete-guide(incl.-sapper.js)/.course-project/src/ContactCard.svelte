d158ef26bbb3e5d48d1ce61b2707de3ad9ad40c6
Alexander Jordan <curtjordan93@gmail.com>
2024-03-07 12:27:00 +0100

    Update project, section 2, chapter 22

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/ContactCard.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/ContactCard.svelte
@@ -25,6 +25,10 @@
         height: 100%;
     }
 
+    .thumb-placeholder {
+        background: #ccc;
+    }
+
     img {
         width: 100%;
         height: 100%;
@@ -61,7 +65,7 @@
 
 <div class="contact-card">
     <header>
-        <div class="thumb">
+        <div class="thumb" class:thumb-placeholder="{!userImgSrc}">
             <img src="{userImgSrc}" alt="{name}">
         </div>
         <div class="user-data">
