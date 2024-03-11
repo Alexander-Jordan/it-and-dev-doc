6ff3ca78f5200a844918ca1647d78711dd0809d5
Alexander Jordan <curtjordan93@gmail.com>
2024-03-11 11:59:34 +0100

    Add assignment project 2, init

---
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.assignment-project-2/src/App.svelte
@@ -0,0 +1,12 @@
+<h1>Assignment</h1>
+
+<p>Solve these tasks.</p>
+
+<ol>
+	<li>Add a password input field and save the user input in a variable.</li>
+	<li>Output "Too short" if the password is shorter than 5 characters and "Too long" if it's longer than 10.</li>
+	<li>Output the password in a paragraph tag if it's between these boundaries.</li>
+	<li>Add a button and let the user add the passwords to an array.</li>
+	<li>Output the array values (= passwords) in a unordered list (ul tag).</li>
+	<li>Bonus: If a password is clicked, remove it from the list.</li>
+</ol>
