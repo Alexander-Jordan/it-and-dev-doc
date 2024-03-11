8e69af047b3b22ddca79f357eb8a3330209a707e
Alexander Jordan <curtjordan93@gmail.com>
2024-03-11 08:36:59 +0100

    Solution to assignment

---
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.assignment-problem-1/src/CourseGoal.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let courseGoal = 'My course goal';
+
+    $: shouldHighlight = courseGoal.includes('!');
+</script>
+
+<style>
+    .highlight {
+        color: #f00
+    }
+</style>
+
+<h1 class:highlight={shouldHighlight}>{courseGoal}</h1>
+  
