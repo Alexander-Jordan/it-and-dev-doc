a101ce28b04ea4f04a1918c1482951cc776c3fd7
Alexander Jordan <curtjordan93@gmail.com>
2024-03-11 07:51:50 +0100

    Add assignment project 1, base

---
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.assignment-problem-1/src/App.svelte
@@ -0,0 +1,14 @@
+<h1>Assignment</h1>
+
+<p>Solve these tasks.</p>
+
+<ol>
+  <li>Add an input field that allows users to enter a course goal.</li>
+  <li>Output the user input in a h1 tag.</li>
+  <li>
+    Color the output red (e.g. by adding a class) if it contains at least one exclamation mark.
+  </li>
+  <li>
+    Put the h1 tag + output into a separate component to which you pass the user input
+  </li>
+</ol>
