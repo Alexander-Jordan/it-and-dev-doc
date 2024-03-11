c343b58ab10437f0a62cad8557cfe457cfe7bd69
Alexander Jordan <curtjordan93@gmail.com>
2024-03-11 12:26:07 +0100

    Assignment solution

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.assignment-project-2/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.assignment-project-2/src/App.svelte
@@ -1,3 +1,26 @@
+<script>
+	let password = '';
+	let passwords = [];
+
+	$: passwordTooShort = password.trim().length < 5;
+	$: passwordTooLong = password.trim().length > 10;
+
+	function addPassword() {
+		if (passwordTooShort || passwordTooLong) {
+			return;
+		}
+
+		passwords = [
+			...passwords,
+			password
+		]
+	}
+
+	function removePassword(index) {
+		passwords = passwords.filter((pwd, i) => i !== index);
+	}
+</script>
+
 <h1>Assignment</h1>
 
 <p>Solve these tasks.</p>
@@ -10,3 +33,19 @@
 	<li>Output the array values (= passwords) in a unordered list (ul tag).</li>
 	<li>Bonus: If a password is clicked, remove it from the list.</li>
 </ol>
+
+<input type="password" bind:value={password}>
+<button on:click={addPassword}>Add Password</button>
+{#if passwordTooShort}
+	<p>Password: Too short</p>
+{:else if passwordTooLong}
+	<p>Password: Too long</p>
+{:else}
+	<p>Password: {password}</p>
+{/if}
+
+<ul>
+	{#each passwords as pwd, i}
+		<li on:click={removePassword.bind(this, i)}>{pwd}</li>
+	{/each}
+</ul>
