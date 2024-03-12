594a8440e7e64ac11fb06fee5cf0b4684369e781
Alexander Jordan <curtjordan93@gmail.com>
2024-03-12 08:08:21 +0100

    Add actual course project, and rename the existing project folder to

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/App.svelte
@@ -1,93 +1,9 @@
 <script>
-	import ContactCard from "./ContactCard.svelte";
 
-	let name = "Alex";
-	let title = "Developer";
-	let image = "https://avatars.githubusercontent.com/u/68730459";
-	let description = "This is a short description about me.";
-	let formState = 'empty';
-
-	let createdContacts = [];
-
-	function addContact() {
-		if (
-			name.trim().length === 0
-			|| title.trim().length === 0
-			|| image.trim().length === 0
-			|| description.trim().length === 0
-		) {
-			formState = 'invalid';
-			return;
-		}
-
-		createdContacts = [
-			...createdContacts,
-			{
-				id: Math.random(),
-				name: name,
-				jobTitle: title,
-				imageUrl: image,
-				desc: description
-			}
-		];
-
-		formState = 'done';
-	}
-
-	function removeFirst() {
-		createdContacts = createdContacts.slice(1);
-	}
-
-	function removeLast() {
-		createdContacts = createdContacts.slice(0, -1);
-	}
-	
 </script>
 
 <style>
-	#form {
-		width: 30rem;
-		max-width: 100%;
-	}
+
 </style>
 
-<div id="form">
-	<div class="form-control">
-		<label for="userName">User Name</label>
-		<input type="text" bind:value={name} id="userName" />
-	</div>
-	<div class="form-control">
-		<label for="jobTitle">Job Title</label>
-		<input type="text" bind:value={title} id="jobTitle" />
-	</div>
-	<div class="form-control">
-		<label for="image">Image URL</label>
-		<input type="text" bind:value={image} id="image" />
-	</div>
-	<div class="form-control">
-		<label for="desc">Description</label>
-		<textarea rows="3" bind:value={description} id="desc" />
-	</div>
-</div>
-
-<button on:click={addContact}>Add Contact Card</button>
-<button on:click={removeFirst}>Remove First</button>
-<button on:click={removeLast}>Remove Last</button>
-
-{#if formState === 'invalid'}
-	<p>Invalid input.</p>
-{:else}
-	<p>Please enter some data and click the button!</p>
-{/if}
-
-{#each createdContacts as contact, i (contact.id)}
-	<h2># {i + 1}</h2>
-	<ContactCard
-		userName={contact.name}
-		jobTitle={contact.jobTitle}
-		description={contact.desc}
-		userImage={contact.imageUrl}
-	/>
-{:else}
-	<p>Please start adding some contacts, we found none!</p>
-{/each}
+<h1>Time for a project!</h1>
