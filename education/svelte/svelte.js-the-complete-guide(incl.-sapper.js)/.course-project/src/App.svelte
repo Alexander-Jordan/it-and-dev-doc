97b444c73724a68eac6ed11948d42eb43e70e446
Alexander Jordan <curtjordan93@gmail.com>
2024-03-12 09:05:35 +0100

    Update course project, section 5, chapter 44

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/App.svelte
@@ -1,5 +1,26 @@
 <script>
   import Header from "./UI/Header.svelte";
+
+  const meetups = [
+    {
+        id: 'm1',
+        title: 'Coding Bootcamp',
+        subtitle: 'Learn to code in 2 hours',
+        description: 'In this meetup, we will have some experts that teach you how to code!',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/JavaScript_code.png/640px-JavaScript_code.png',
+        address: '27th Nerd Road, 32523 New York',
+        contactEmail: 'code@test.com'
+    },
+    {
+        id: 'm2',
+        title: 'Swim Together',
+        subtitle: 'Let\'s go for some swimming',
+        description: 'We will simply swim a bit!',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Polar_bear_swimming.jpg/640px-Polar_bear_swimming.jpg',
+        address: '2nd Beach Avenue, 13231 New Zealand',
+        contactEmail: 'swim@test.com'
+    }
+  ]
 </script>
 
 <Header />
