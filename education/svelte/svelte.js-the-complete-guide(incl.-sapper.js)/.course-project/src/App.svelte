231c43a1e3e1751aff4692bfa1d190adbbdc8b8e
Alexander Jordan <curtjordan93@gmail.com>
2024-03-13 11:10:45 +0100

    Update course project, section 5, chapter 50

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
     import MeetupGrid from "./Meetups/MeetupGrid.svelte";
     import Header from "./UI/Header.svelte";
+    import TextInput from "./UI/TextInput.svelte";
 
     let title = '';
     let subtitle = '';
@@ -49,36 +50,56 @@
     main {
         margin-top: 5rem;
     }
+
+    form {
+        width: 30rem;
+        max-width: 90%;
+        margin: auto;
+    }
 </style>
 
 <Header />
 
 <main>
     <form on:submit|preventDefault={addMeetup}>
-        <div class="form-control">
-            <label for="title">Title</label>
-            <input type="text" id="title" bind:value={title}>
-        </div>
-        <div class="form-control">
-            <label for="subtitle">Subtitle</label>
-            <input type="text" id="subtitle" bind:value={subtitle}>
-        </div>
-        <div class="form-control">
-            <label for="address">Address</label>
-            <input type="text" id="address" bind:value={address}>
-        </div>
-        <div class="form-control">
-            <label for="imageUrl">Image URL</label>
-            <input type="text" id="imageUrl" bind:value={imageUrl}>
-        </div>
-        <div class="form-control">
-            <label for="email">E-Mail</label>
-            <input type="text" id="email" bind:value={email}>
-        </div>
-        <div class="form-control">
-            <label for="description">Description</label>
-            <textarea rows="3" id="description" bind:value={description}></textarea>
-        </div>
+        <TextInput
+            id="title"
+            label="Title"
+            value={title}
+            on:input={(event) => title = event.target.value}
+        />
+        <TextInput
+            id="subtitle"
+            label="Subtitle"
+            value={subtitle}
+            on:input={(event) => subtitle = event.target.value}
+        />
+        <TextInput
+            id="address"
+            label="Address"
+            value={address}
+            on:input={(event) => address = event.target.value}
+        />
+        <TextInput
+            id="imageUrl"
+            label="Image URL"
+            value={imageUrl}
+            on:input={(event) => imageUrl = event.target.value}
+        />
+        <TextInput
+            id="email"
+            label="E-Mail"
+            inputType="email"
+            value={email}
+            on:input={(event) => email = event.target.value}
+        />
+        <TextInput
+            id="description"
+            label="Description"
+            controlType="textarea"
+            value={description}
+            on:input={(event) => description = event.target.value}
+        />
         <button type="submit">Save</button>
     </form>
 
---
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/UI/TextInput.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let controlType = 'input';
+    export let id;
+    export let label;
+    export let rows = '3';
+    export let value;
+    export let inputType = 'text';
+</script>
+
+<style>
+    input,
+    textarea {
+        display: block;
+        width: 100%;
+        font: inherit;
+        border: none;
+        border-bottom: 2px solid #ccc;
+        border-radius: 3px 3px 0 0;
+        background: white;
+        padding: 0.15rem 0.25rem;
+        transition: border-color 0.1s ease-out;
+    }
+
+    input:focus,
+    textarea:focus {
+        border-color: #e40763;
+        outline: none;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 0.5rem;
+        width: 100%;
+    }
+
+    .form-control {
+        padding: 0.5rem 0;
+        width: 100%;
+        margin: 0.25rem 0;
+    }
+</style>
+
+<div class="form-control">
+    <label for={id}>{label}</label>
+    {#if controlType === 'textarea'}
+        <textarea rows={rows} id={id} {value} on:input></textarea>
+    {:else}
+        <input type={inputType} id={id} {value} on:input>
+    {/if}
+</div>
