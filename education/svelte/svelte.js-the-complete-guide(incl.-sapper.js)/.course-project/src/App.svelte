862cbf79623ba08e7d50ef1d7ec107bd1bc7aa13
Alexander Jordan <curtjordan93@gmail.com>
2024-03-13 08:51:39 +0100

    Update course project, section 5, chapter 48

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/App.svelte
@@ -1,46 +1,37 @@
 <script>
-  import MeetupItem from "./Meetups/MeetupItem.svelte";
-import Header from "./UI/Header.svelte";
+    import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+    import Header from "./UI/Header.svelte";
 
-  const meetups = [
-    {
-        id: 'm1',
-        title: 'Coding Bootcamp',
-        subtitle: 'Learn to code in 2 hours',
-        description: 'In this meetup, we will have some experts that teach you how to code!',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/JavaScript_code.png/640px-JavaScript_code.png',
-        address: '27th Nerd Road, 32523 New York',
-        contactEmail: 'code@test.com'
-    },
-    {
-        id: 'm2',
-        title: 'Swim Together',
-        subtitle: 'Let\'s go for some swimming',
-        description: 'We will simply swim a bit!',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Polar_bear_swimming.jpg/640px-Polar_bear_swimming.jpg',
-        address: '2nd Beach Avenue, 13231 New Zealand',
-        contactEmail: 'swim@test.com'
-    }
-  ]
+    const meetups = [
+        {
+            id: 'm1',
+            title: 'Coding Bootcamp',
+            subtitle: 'Learn to code in 2 hours',
+            description: 'In this meetup, we will have some experts that teach you how to code!',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/JavaScript_code.png/640px-JavaScript_code.png',
+            address: '27th Nerd Road, 32523 New York',
+            contactEmail: 'code@test.com'
+        },
+        {
+            id: 'm2',
+            title: 'Swim Together',
+            subtitle: 'Let\'s go for some swimming',
+            description: 'We will simply swim a bit!',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Polar_bear_swimming.jpg/640px-Polar_bear_swimming.jpg',
+            address: '2nd Beach Avenue, 13231 New Zealand',
+            contactEmail: 'swim@test.com'
+        }
+    ]
 </script>
 
 <style>
-    #meetups {
+    main {
         margin-top: 5rem;
     }
 </style>
 
 <Header />
 
-<section id="meetups">
-    {#each meetups as meetup}
-        <MeetupItem
-            title={meetup.title}
-            subtitle={meetup.subtitle}
-            description={meetup.description}
-            imageUrl={meetup.imageUrl}
-            address={meetup.address}
-            email={meetup.contactEmail}
-        />
-    {/each}
-</section>
+<main>
+    <MeetupGrid {meetups}/>
+</main>
