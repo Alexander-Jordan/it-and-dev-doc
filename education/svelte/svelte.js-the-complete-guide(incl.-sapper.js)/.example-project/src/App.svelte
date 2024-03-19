5aeda0722d55579f758909395c0e66a6d33cb439
Alexander Jordan <curtjordan93@gmail.com>
2024-03-19 09:07:06 +0100

    Update example project, chapter 59

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
@@ -1,9 +1,17 @@
 <script>
 	import Product from './Product.svelte'
+
+	function addToCart(event) {
+		console.log(event.detail);
+	}
+
+	function deleteProduct(event) {
+		console.log(event.detail);
+	}
 </script>
 
 <Product
 	productTitle='A Book'
-	on:add-to-cart={() => alert('Add to cart!')}
-	on:delete={() => alert('Delete!')}
+	on:add-to-cart={addToCart}
+	on:delete={deleteProduct}
 />
