0b137caed6721ad3d54e99f5adec85fca733ea39
Alexander Jordan <curtjordan93@gmail.com>
2024-03-19 11:26:04 +0100

    Example, chapter 66

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/Modal.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/Modal.svelte
@@ -1,9 +1,32 @@
 <script>
-    import { createEventDispatcher } from "svelte";
+    import {
+        createEventDispatcher,
+        onMount,
+        onDestroy,
+        beforeUpdate,
+        afterUpdate
+    } from "svelte";
 
     let dispatch = createEventDispatcher();
 
     let agreed = false;
+    let autoscroll = false;
+
+    onMount(() => console.log('onMount'));
+    onDestroy(() => console.log('onDestroy'));
+    beforeUpdate(() => {
+        console.log('beforeUpdate')
+        autoscroll = agreed;
+    });
+    afterUpdate(() => {
+        console.log('afterUpdate')
+        if (autoscroll) {
+            const modal = document.querySelector('.modal');
+            modal.scrollTo(0, modal.scrollHeight)
+        }
+    });
+
+    console.log('Script executed');
 </script>
 
 <style>
@@ -23,7 +46,7 @@
         top: 10vh;
         left: 10%;
         width: 80%;
-        max-height: 80vh;
+        max-height: 15vh;
         background: white;
         border-radius: 5px;
         z-index: 100;
