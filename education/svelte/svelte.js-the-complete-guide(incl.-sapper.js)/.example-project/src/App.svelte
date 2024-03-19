cae07acb23375bacf8b1d01ba52501389506869e
Alexander Jordan <curtjordan93@gmail.com>
2024-03-19 08:50:47 +0100

    Example project, chapter 58

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
@@ -2,4 +2,8 @@
 	import Product from './Product.svelte'
 </script>
 
-<Product productTitle='A Book' on:click={() => alert('Clicked!')}/>
+<Product
+	productTitle='A Book'
+	on:add-to-cart={() => alert('Add to cart!')}
+	on:delete={() => alert('Delete!')}
+/>
--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/Product.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/Product.svelte
@@ -1,9 +1,17 @@
 <script>
+    import { createEventDispatcher } from 'svelte';
+
     export let productTitle;
+
+    const dispatch = createEventDispatcher();
+
+    function addToCart() {
+        dispatch('add-to-cart', {id: 'p1'});
+    }
 </script>
 
 <article>
     <h1>{productTitle}</h1>
-    <button on:click>Add to Cart</button>
-    <button on:click>Delete</button>
+    <button on:click={addToCart}>Add to Cart</button>
+    <button on:click={() => dispatch('delete', 'p1')}>Delete</button>
 </article>
