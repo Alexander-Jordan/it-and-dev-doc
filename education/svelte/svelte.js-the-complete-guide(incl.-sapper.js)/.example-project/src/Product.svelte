904a8a55f0899a60b35e0b8f1adf23e8a8799e40
Alexander Jordan <curtjordan93@gmail.com>
2024-03-19 08:27:43 +0100

    Example project, chapter 57

---
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/Product.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let productTitle;
+</script>
+
+<article>
+    <h1>{productTitle}</h1>
+    <button on:click>Add to Cart</button>
+    <button on:click>Delete</button>
+</article>
