f82ea328ad70335bf2203b8ded72f7ef73c61aea
Alexander Jordan <curtjordan93@gmail.com>
2024-03-26 15:51:10 +0100

    Example project

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
@@ -1,9 +1,14 @@
 <script>
+	import CustomInput from "./CustomInput.svelte";
+  	import Toggle from "./Toggle.svelte";
+
 	let val = '';
 	let value = '';
+	let selectedOption = 1;
 
 	$: console.log(val);
 	$: console.log(value);
+	$: console.log(selectedOption)
 
 	function setValue(event) {
 		val = event.target.value;
@@ -13,3 +18,5 @@
 <input type="text" value={val} on:input={setValue}>
 <input type="text" bind:value={val}>
 <input type="text" bind:value>
+<CustomInput bind:val={val} />
+<Toggle bind:chosenOption={selectedOption} />
---
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/CustomInput.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let val;
+    export let type = 'text';
+
+    function setVal(event) {
+        val = event.target.value;
+    }
+</script>
+
+<input type={type} value={val} on:input={setVal}>
