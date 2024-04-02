0373e7c7ab133a92112258fc32a438d82b47cba1
Alexander Jordan <curtjordan93@gmail.com>
2024-04-02 10:29:48 +0200

    Update example project, section 10, chapter 106

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/Cart/Cart.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/Cart/Cart.svelte
@@ -1,19 +1,6 @@
 <script>
-  import { onDestroy } from 'svelte'
   import cartItems from './cart-store.js';
   import CartItem from "./CartItem.svelte";
-
-  let items;
-
-  const unsubscribe = cartItems.subscribe(ci => {
-    items = ci;
-  });
-
-  onDestroy(() => {
-    if (unsubscribe) {
-      unsubscribe();
-    }
-  })
 </script>
 
 <style>
@@ -34,7 +21,7 @@
 <section>
   <h1>Cart</h1>
   <ul>
-    {#each items as item (item.id)}
+    {#each $cartItems as item (item.id)}
       <CartItem id={item.id} title={item.title} price={item.price} />
     {:else}
       <p>No items in cart yet!</p>
