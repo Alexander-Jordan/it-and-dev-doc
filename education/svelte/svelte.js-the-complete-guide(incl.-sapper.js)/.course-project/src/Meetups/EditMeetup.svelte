0479df1d2927865821a316e9e7e8d9e54b5d7728
Alexander Jordan <curtjordan93@gmail.com>
2024-04-02 07:44:45 +0200

    Update course project, section 9, chapter 95

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/Meetups/EditMeetup.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/Meetups/EditMeetup.svelte
@@ -3,8 +3,10 @@
     import TextInput from "../UI/TextInput.svelte";
     import Button from "../UI/Button.svelte";
     import Modal from "../UI/Modal.svelte";
+    import { isEmpty } from '../helpers/validation'
 
     let title = '';
+    let titleValid = false;
     let subtitle = '';
     let address = '';
     let email = '';
@@ -12,6 +14,8 @@
     let imageUrl = '';
 
     const dispatch = createEventDispatcher();
+
+    $: titleValid = !isEmpty(title);
 
     function cancelForm() {
         dispatch('cancel');
@@ -40,7 +44,7 @@
         <TextInput
             id="title"
             label="Title"
-            valid={true}
+            valid={titleValid}
             validityMessage="Please enter a valid title."
             value={title}
             on:input={(event) => title = event.target.value}
---
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/helpers/validation.js
@@ -0,0 +1,3 @@
+export function isEmpty(val) {
+    return val.trim().length === 0;
+}
