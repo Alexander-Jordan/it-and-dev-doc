264f0e2e3be78c8da23d296952de2e0a06fa9c82
Alexander Jordan <curtjordan93@gmail.com>
2024-04-08 09:41:58 +0200

    Update course project, section 11, chapter 123

---
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/Meetups/MeetupFilter.svelte
@@ -0,0 +1,65 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let selectedButton = 0;
+</script>
+
+<style>
+    div {
+        font-size: 0px;
+    }
+
+    button {
+        background: #aaaaaa;
+        font: inherit;
+        font-size: 1rem;
+        cursor: pointer;
+        border: 1px solid #aaaaaa;
+        padding: 0.5rem 1rem;
+    }
+
+    button:focus {
+        outline: none;
+    }
+
+    button:first-of-type {
+        border-radius: 5px 0 0 5px;
+    }
+
+    button:last-of-type {
+        border-radius: 0 5px 5px 0;
+    }
+
+    button:hover,
+    button:active,
+    .active {
+        background: #cf0056;
+        border-color: #cf0056;
+        color: white;
+    }
+</style>
+
+<div>
+    <button
+        type="button"
+        class:active={selectedButton === 0}
+        on:click={() => {
+            selectedButton = 0;
+            dispatch('select', 0);
+        }}
+    >
+        All
+    </button>
+    <button
+        type="button"
+        class:active={selectedButton === 1}
+        on:click={() => {
+            selectedButton = 1;
+            dispatch('select', 1);
+        }}
+    >
+        Favorites
+    </button>
+</div>
--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/Meetups/MeetupGrid.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/Meetups/MeetupGrid.svelte
@@ -1,26 +1,43 @@
 <script>
+    import MeetupFilter from "./MeetupFilter.svelte";
     import MeetupItem from "./MeetupItem.svelte";
 
     export let meetups;
+
+    let favoritesOnly = false;
+
+    $: filteredMeetups = favoritesOnly ? meetups.filter(m => m.isFavorite) : meetups;
+
+    function setFilter(event) {
+        favoritesOnly = event.detail === 1;
+    }
 </script>
 
 <style>
-    section {
+    #meetups {
         width: 100%;
         display: grid;
         grid-template-columns: 1fr;
         grid-gap: 1rem;
     }
 
+    #meetup-controls {
+        margin: 1rem;
+    }
+
     @media (min-width: 768px) {
-        section {
+        #meetups {
             grid-template-columns: repeat(2, 1fr);
         }
     }
 </style>
 
+<section id="meetup-controls">
+    <MeetupFilter on:select={setFilter} />
+</section>
+
 <section id="meetups">
-    {#each meetups as meetup}
+    {#each filteredMeetups as meetup}
         <MeetupItem
             id={meetup.id}
             title={meetup.title}
