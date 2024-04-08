0f23727c668a3c403fbf9ceffb386a6470820f2b
Alexander Jordan <curtjordan93@gmail.com>
2024-04-08 13:17:40 +0200

    Update example project, section 12, chapter 128

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
@@ -1,2 +1,16 @@
 <script>
+    import { tweened } from 'svelte/motion';
+    import { cubicIn } from 'svelte/easing';
+
+    const progress = tweened(0, {
+        delay: 0, // how long before the animation starts
+        duration: 700, // duration of the animation in ms (default: 400)
+        easing: cubicIn, // easing function (svelte has some built-in)
+    });
+
+    setTimeout(() => {
+        progress.set(0.5);
+    }, 1500);
 </script>
+
+<progress value={$progress}></progress>
