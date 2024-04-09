cc8d517972cefa63dab4ba084a94e3d872b598b0
Alexander Jordan <curtjordan93@gmail.com>
2024-04-09 13:43:15 +0200

    Update example project, section 14, chapter 145

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.example-project/src/App.svelte
@@ -1,3 +1,35 @@
 <script>
-    
+    let hobbies = [];
+    let hobbyInput;
+
+    function addHobby() {
+        hobbies = [...hobbies, hobbyInput.value];
+
+        fetch(
+            'https://svelte-project-5e789-default-rtdb.firebaseio.com/hobbies.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(hobbies),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(response => {
+            if (!response.ok) {
+                throw new Error('Failed');
+            }
+        }).catch(error => {
+            console.log(error)
+        });
+    }
 </script>
+
+<label for="hobby">Hobby</label>
+<input type="text" id="hobby" bind:this={hobbyInput}>
+<button on:click={addHobby}>Add Hobby</button>
+
+<ul>
+    {#each hobbies as hobby}
+        <li>{hobby}</li>
+    {/each}
+</ul>
