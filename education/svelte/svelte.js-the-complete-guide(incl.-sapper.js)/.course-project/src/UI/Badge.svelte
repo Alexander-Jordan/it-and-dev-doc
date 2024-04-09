270abf92c4a0f99f78d45f6e4fba4fd906413bd1
Alexander Jordan <curtjordan93@gmail.com>
2024-04-09 12:48:39 +0200

    Update course project, section 13, chapter 141

--- education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/UI/Badge.svelte
+++ education/svelte/svelte.js-the-complete-guide(incl.-sapper.js)/.course-project/src/UI/Badge.svelte
@@ -1,3 +1,7 @@
+<script>
+    import { slide } from "svelte/transition";
+</script>
+
 <style>
     span {
         display: inline-block;
@@ -12,4 +16,4 @@
     }
 </style>
 
-<span><slot /></span>
+<span transition:slide><slot /></span>
